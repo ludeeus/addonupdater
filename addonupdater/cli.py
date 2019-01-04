e6c9c4262fd8782050ad7eff026d17619815587a
ludeeus <joasoe@gmail.com>
2019-01-04 12:36:51 +0100

    skip are flags

--- addonupdater/cli.py
+++ addonupdater/cli.py
@@ -9,9 +9,9 @@ import click
 @click.option('--test', is_flag=True, help="Test run, will not commit.")
 @click.option('--verbose', is_flag=True, help="Print more stuff.")
 @click.option('--release', default=None, help="Publish a release.")
-@click.option('--skip_apk', default=None, help="Skip apk updates.")
-@click.option('--skip_pip', default=None, help="Skip pip updates.")
-@click.option('--skip_custom', default=None, help="Skip custom updates.")
+@click.option('--skip_apk', is_flag=True, help="Skip apk updates.")
+@click.option('--skip_pip', is_flag=True, help="Skip pip updates.")
+@click.option('--skip_custom', is_flag=True, help="Skip custom updates.")
 def cli(token, addon, repo, test, verbose, release,
         skip_apk, skip_pip, skip_custom):
     """CLI for this package."""
