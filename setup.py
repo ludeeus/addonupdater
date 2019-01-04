967198d8ffab121408becd1b5c171f270f88d925
ludeeus <joasoe@gmail.com>
2019-01-04 12:40:44 +0100

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="addonupdater",
-    version="0.0.12",
+    version="0.0.13",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
