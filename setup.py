307e02e19879b664ac37332a694f489902c1b607
ludeeus <joasoe@gmail.com>
2019-02-13 19:59:20 +0100

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="addonupdater",
-    version="0.1.1",
+    version="0.1.2",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
